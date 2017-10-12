7395ee7bcd8baf8c484bcfed81841c1ce3ddb34b
Sergei Volodin <etoestja@yandex.ru>
2017-10-12 15:20:52 +0200

    Add ex04:1

--- labs/ex04/template/plots.py
+++ labs/ex04/template/plots.py
@@ -4,10 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def cross_validation_visualization(lambds, mse_tr, mse_te):
+def cross_validation_visualization(lambds, mse_tr, mse_te, all_data = None):
     """visualization the curves of mse_tr and mse_te."""
-    plt.semilogx(lambds, mse_tr, marker=".", color='b', label='train error')
-    plt.semilogx(lambds, mse_te, marker=".", color='r', label='test error')
+    plt.figure()
+    plt.xscale("log")
+    plt.plot(lambds, mse_tr, marker=".", color='b', label='train error')
+    plt.plot(lambds, mse_te, marker=".", color='r', label='test error')
+    if type(all_data) != type(None):
+        widths = (lambds - np.hstack([lambds[1:], [0.5]])) / 2.
+        plt.boxplot(all_data[0], positions = lambds, widths = widths, manage_xticks = False)
+        plt.boxplot(all_data[1], positions = lambds, widths = widths, manage_xticks = False)
     plt.xlabel("lambda")
     plt.ylabel("rmse")
     plt.title("cross validation")
