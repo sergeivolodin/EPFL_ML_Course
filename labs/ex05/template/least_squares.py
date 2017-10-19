5a94009ee42d7fc440de0d304efc93a0ee353005
Sergei Volodin <etoestja@yandex.ru>
2017-10-19 14:26:54 +0200

    Add least squares implementation

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -1,17 +1,1 @@
-# -*- coding: utf-8 -*-
-"""Exercise 3.
-
-Least Square
-"""
-
-import numpy as np
-
-
-def least_squares(y, tx):
-    """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+../../ex04/template/least_squares.py
